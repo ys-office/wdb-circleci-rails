8288523c662b4ca84f63543647449b8a6ce1cfa4
tomoya <tomoya.ton@gmail.com>
2018-09-27 16:01:35 +0900

    Modify CreateTodos migration and create schema file

--- db/migrate/20180927065945_create_todos.rb
+++ db/migrate/20180927065945_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
-      t.string :title
-      t.boolean :done
+      t.string :title, null: false, default: ''
+      t.boolean :done, null: false, default: false
 
       t.timestamps
     end
